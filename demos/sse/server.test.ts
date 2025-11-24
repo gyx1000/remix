7a0c0de4eb0d12c0d89ef8b989278c8f83c08558
Guillaume Pannatier <guillaume.pannatier@gmail.com>
2025-11-24 18:14:57 +0100

    wip - simplfiy failing test

--- demos/sse/server.test.ts
+++ demos/sse/server.test.ts
@@ -1,11 +1,7 @@
 import { createRouter } from '@remix-run/fetch-router'
-import { createRequestListener } from '@remix-run/node-fetch-server'
 import { createSseSession } from '@remix-run/sse'
-import { once } from 'events'
-import { createServer } from 'http'
 import { describe, it } from 'node:test'
 import * as res from '@remix-run/fetch-router/response-helpers'
-import type { AddressInfo } from 'net'
 
 describe('Test sse endpoint', async () => {
   it('should abort request correctly', async (t) => {
@@ -17,7 +13,7 @@ describe('Test sse endpoint', async () => {
 
       let interval = setInterval(() => {
         sse.push('message', Date.now().toString())
-      })
+      }, 200)
 
       request.signal.addEventListener('abort', () => {
         console.log('fully aborted !')
@@ -27,36 +23,29 @@ describe('Test sse endpoint', async () => {
       return res.sse(sse.stream)
     })
 
-    // run the nodejs server
-    let server = createServer(
-      createRequestListener(async (request) => {
-        try {
-          request.signal.addEventListener('abort', () => {
-            console.log(`Aborted there !`)
-          })
-          return await router.fetch(request)
-        } catch (error) {
-          console.log(error)
-          return new Response(`Internal Error`, { status: 500 })
-        }
-      }),
-    ).listen(0)
-    t.after(() => server.close())
+    let ac = new AbortController()
+    let req = new Request('http://localhost/sse', { signal: ac.signal })
 
-    // simulate a client request
-    const ac = new AbortController()
-    let req = new Request(`http://localhost:${(server.address() as AddressInfo).port}/sse`, {
-      signal: ac.signal,
-    })
     setTimeout(() => {
       // force GC
       global.gc!()
       // client cancel the request
+      console.log('abort the request')
       ac.abort()
-    }, 500)
+    })
 
-    await once(server, 'listening')
-    let response = await fetch(req)
+    let response = await router.fetch(req)
     t.assert.equal(response.status, 200)
+    if (response.body) {
+      let reader = response.body.getReader()
+      try {
+        while (true) {
+          let result = await reader.read()
+          if (result.done) break
+        }
+      } finally {
+        reader.releaseLock()
+      }
+    }
   })
 })
